261d1486c3054837142340b26b2e2583efec5970
zhengjuanxia <zhengjuanxia@sina.cn>
2018-08-01 16:14:51 +0800

    开篇——第一个基于SpringBoot2.0的Web工程

---
+++ 1_boot_start/src/main/java/com/example/boot_start/BootStartApplication.java
@@ -0,0 +1,12 @@
+package com.example.boot_start;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootStartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootStartApplication.class, args);
+    }
+}
